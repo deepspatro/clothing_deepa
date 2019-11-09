5a9d223bf059b67cb829a8ccbf574fea1fdd8bae
Deepanjali <deepanjali.patro@gmail.com>
2019-11-10 01:03:17 +0530

    Created Crown clothing project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import HomePage from './homepage.components';
+
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
