a7fdf2a0f9d10cb64bd979ddb3f0eaffd8287c1d
Deepanjali <deepanjali.patro@gmail.com>
2019-12-06 17:40:38 +0530

    integrated firebase with sign up component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ShopPage from './Components/pages/shop/shop.components';
 import Header from './Components/header/header.component';
 import SignInAndSignUpPage from './Components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import './App.css';
 
@@ -24,8 +24,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          }, () => { console.log(this.state); })
+        })
+
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
+
+
     })
   }
 
